440a224419569b45a5358623269678880ef82c40
Tanvir Ahmad <tnvir9@gmail.com>
2024-05-05 17:07:10 +0600

    Create About section with image Relative position.

---
+++ src/Pages/Home/About/About.jsx
@@ -0,0 +1,35 @@
+import parts from '../../../assets/images/about_us/parts.jpg';
+import person from '../../../assets/images/about_us/person.jpg';
+const About = () => {
+  return (
+    <div className="hero min-h-screen bg-base-200 my-24 rounded-xl">
+      <div className="hero-content flex-col lg:flex-row ">
+        <div className='w-1/2 relative'>
+        <img
+          src={person}
+          className="w-3/4 rounded-lg shadow-2xl"
+        />
+        <img
+          src={parts}
+          className="w-1/2 rounded-lg shadow-2xl absolute top-1/2 right-2 border-8 border-white"
+        />
+        </div>
+        <div className='w-1/2'>
+          <p className="text-5 text-[#FF3811] font-bold mb-5">About Us</p>
+            <h1 className='text-5xl font-bold text-[#151515] w-3/4 mb-8'>
+            We are qualified & of experience in this field
+            </h1>
+          <p className="text-[#737373] capitalize mb-5">
+          There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. 
+          </p>
+          <p className="text-[#737373] capitalize mb-8">
+          the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable.   
+          </p>
+          <button className="btn bg-[#FF3811] text-[#FFFFFF]">Get More Info</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
   return (
     // <div className="slider-container">
 
-    <div className="carousel w-full h-[600px] rounded-xl">
+    <div className="carousel w-full h-[600px] rounded-xl mt-16">
       <div id="slide1" className="carousel-item relative w-full ">
         <img src={img1} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
@@ -40,7 +40,7 @@ const Banner = () => {
       <div id="slide2" className="carousel-item relative w-full">
         <img src={img2} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
-          <h1 className="text-6xl font-bold w-1/">
+          <h1 className="text-6xl font-bold w-1/3">
             Affordable Price For Car Servicing
           </h1>
           <p className="w-1/3">
@@ -66,7 +66,7 @@ const Banner = () => {
       <div id="slide3" className="carousel-item relative w-full">
         <img src={img3} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
-          <h1 className="text-6xl font-bold w-1/">
+          <h1 className="text-6xl font-bold w-1/3">
             Affordable Price For Car Servicing
           </h1>
           <p className="w-1/3">
@@ -92,7 +92,7 @@ const Banner = () => {
       <div id="slide4" className="carousel-item relative w-full">
         <img src={img4} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
-          <h1 className="text-6xl font-bold w-1/">
+          <h1 className="text-6xl font-bold w-1/3">
             Affordable Price For Car Servicing
           </h1>
           <p className="w-1/3">
@@ -118,7 +118,7 @@ const Banner = () => {
       <div id="slide5" className="carousel-item relative w-full">
         <img src={img5} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
-          <h1 className="text-6xl font-bold w-1/">
+          <h1 className="text-6xl font-bold w-1/3">
             Affordable Price For Car Servicing
           </h1>
           <p className="w-1/3">
@@ -144,7 +144,7 @@ const Banner = () => {
       <div id="slide6" className="carousel-item relative w-full">
         <img src={img6} className="w-full object-cover animate__animated animate__zoomIn" />
         <div className="absolute flex flex-col justify-center items-start left-0 top-0 text-white bg-gradient-to-l from-[#15151500] to-[#151515] h-full w-full pl-10 space-y-4 ">
-          <h1 className="text-6xl font-bold w-1/">
+          <h1 className="text-6xl font-bold w-1/3">
             Affordable Price For Car Servicing
           </h1>
           <p className="w-1/3">
